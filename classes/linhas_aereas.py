cffa0f4f00a590308b110e0a02e6c15c404d264e
thiagopls1 <thiagopls1@hotmail.com>
2025-04-03 15:24:08 -0300

    Create `definir_variaveis` at `classes/linhas_aereas.py`
    Co-Authored-By: Resutemon1 <resutemon@gmail.com>

--- classes/linhas_aereas.py
+++ classes/linhas_aereas.py
@@ -35,10 +35,23 @@ class LinhasAereas(Individuo):
     self.restricoes = SatisfacaoRestricoes(self.variaveis, self.dominios)
 
   def definir_variaveis(self):
-    pass
+    self.variaveis = [
+      "00:00", "00:30", "01:00", "01:30", "02:00", "02:30",
+      "03:00", "03:30", "04:00", "04:30", "05:00", "05:30",
+      "06:00", "06:30", "07:00", "07:30", "08:00", "08:30",
+      "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
+      "12:00", "12:30", "12:00", "12:30", "13:00", "13:30",
+      "14:00", "14:30", "15:00", "15:30", "16:00", "16:30",
+      "17:00", "17:30", "18:00", "18:30", "19:00", "19:30",
+      "20:00", "20:30", "21:00", "21:30", "22:00", "22:30",
+      "23:00", "23:30"
+    ]
+    possiveisRotas = [(x[0], x[1], x[2]) for x in self.totalVoos]
 
-  def definir_dominios(self):
-    pass
+    for variavel in self.variaveis:
+      # As tuplas representam: Origem, Destino, Tempo
+      # (None, None, 0) = Tempo ocioso
+      self.dominios[variavel] = [(None, None, 0), *possiveisRotas]
 
   def mutacao(self):
     pass
@@ -56,7 +69,6 @@ class LinhasAereas(Individuo):
 
   def gerar_genes(self):
     self.definir_variaveis()
-    self.definir_dominios()
     self.genes = self.restricoes.busca_backtracking()
 
 class PopulacaoLinhasAereas(Populacao):
