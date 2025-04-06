f5d4450dbf1578cb1e5724e6c2c8e41c23b62699
viniciusegydio <egydiovinicius022@gmail.com>
2025-04-06 16:26:26 -0300

    finished implementation of main functions

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def calcular_total_voos(): #Visita cada tupla e retorna o valor total de voos
         val+= voo[3]
     return val
 
-pop_tamanho = 300 #Valor escolhido aleatoriamente. Procurar um valor mais adequado depois
+pop_tamanho = 420 #Valor escolhido aleatoriamente. Procurar um valor mais adequado depois
 num_geracoes = 400 #Valor escolhido aleatoriamente. Procurar um valor mais adequado depois
 chance_crossover = 0.5 #Valor escolhido aleatoriamente. Procurar um valor mais adequado depois
 chance_mutacao = 0.3 #Valor escolhido aleatoriamente. Procurar um valor mais adequado depois
@@ -108,24 +108,62 @@ def mutar(individuo, chance_mutacao):
             individuo[i] = random.randint(0, val_max_aeronaves -1) #Altera o gene caso a chance de mutação seja maior que o valor aleatório
     return individuo #Retorna o indivíduo mutado ou não
 
-def crossover(individuo_pai1, individuo_pai2, chance):
-    if random.random() < chance: #Sortei um valor aleatório e ver se é menor que a chance
-        pos_corte = random.randint(1, len(individuo_pai1 -1)) #Não pode cortar na pos 0 ou na ultima pos
-        novo_individuo = individuo_pai1[:pos_corte] + individuo_pai2[pos_corte:]
-        novo_individuo2 = individuo_pai2[:pos_corte] + individuo_pai1[pos_corte:]
-        return novo_individuo, novo_individuo2
-    else:
-        return individuo_pai1[:], individuo_pai2[:]
+def crossover(individuo_pai1, individuo_pai2):
+    if len(individuo_pai1) != len(individuo_pai2):
+        print("Os 2 indivíduos devem ter o mesmo tamanho para que possa ocorrer cross-over")
+        return #Retorna nada
 
-def selecao(populacao, fitness, torneio_len):
-    individuos_selecionados = []
+    ponto = random.randint(1, len(individuo_pai1) - 1) #Não pode ser o primeiro nem último elemento do indivíduo
+    novo_individuo1 = individuo_pai1[:ponto] + individuo_pai2[ponto:]
+    novo_individuo2 = individuo_pai2[:ponto] + individuo_pai1[ponto:]
 
-    for i in range(2):
-        selecao_torneio = random.sample()
+    return novo_individuo1, novo_individuo2
 
-linhas_aereas = LinhasAereas(random.randint(0, 20))
+def selecao(populacao, fitness, num_individuos = 3 , num_individuos_elite = 1): #Obs: o num_individuos = 3 pois é uma valor mais balanceado, com boa diversidade e focado em bons indivíduos
+    prox_geracao = []
+    sorted_populacao = sorted(populacao, key= fitness) #Ordena a popualação a partir da função de fitness
+    elites = sorted_populacao[:num_individuos_elite] #Como a população está ordenada por fitness, garante que pega os melhores indivíduos
+    prox_geracao.extend(elites) #.extend() "funde" duas listas
+
+    while len(prox_geracao) < len(populacao):
+        torneio = random.sample(populacao, num_individuos) #.sample() retorna uma lista com um número especifo de items aleatórios de uma sequência
+        melhor_individuo = min(torneio, key=fitness) #No nosso contexto, o vencedor será o com menor fitness(menos penalidades)
+        prox_geracao.append(melhor_individuo) #Adiciona o melhor indivíduo para a próxima geração
+
+    return prox_geracao
+
+def algoritmo_genetico():
+    populacao = gerar_populacao()
+
+    for geracao in range(num_geracoes):
+        nova_populacao = selecao(populacao, fitness, num_individuos = 3, num_individuos_elite = 2) #Seleção
+        prox_geracao = nova_populacao[:2] #Mantém os 2 melhores indivíduos
+
+        while len(prox_geracao) < pop_tamanho: #Usado para aplicar mutação e crossover
+            pais = random.sample(nova_populacao, 2) #Seleciona 2 pais de maneira aleatória
+
+            if random.random() < chance_crossover: #Crossover:
+                novo_individuo1, nova_individuo2 = crossover(pais[0], pais[1])
+            else:
+                novo_individuo1, nova_individuo2 = pais[0][:], pais[1][:] #Cópia
+
+            #Mutação:
+            novo_individuo1 = mutar(novo_individuo1, chance_mutacao)
+            nova_individuo2 = mutar(nova_individuo2, chance_mutacao)
+
+            prox_geracao.extend([novo_individuo1, nova_individuo2])
+
+        populacao = prox_geracao[:pop_tamanho]
+
+    return min(populacao, key = fitness) #Melhor indivíduo
+
+melhor_solucao = algoritmo_genetico()
+print("Melhor indivíduo:", melhor_solucao)
+print("Fitness final:", fitness(melhor_solucao))
+
+#linhas_aereas = LinhasAereas(random.randint(0, 20))
 # print(linhas_aereas)
-aviao = Aviao(linhas_aereas)
+#aviao = Aviao(linhas_aereas)
 
-for x in range(0, len(aviao.rota)):
-  print(f"{x}:\t{aviao.rota[x]}")
+#for x in range(0, len(aviao.rota)):
+  #print(f"{x}:\t{aviao.rota[x]}")
